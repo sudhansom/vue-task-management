55eea0bb57cc79fb6e3d2f63aa2048e5dae3c763
bhuwan <bkspoudel@gmail.com>
2025-10-06 22:31:34 +0200

    fix styling

--- AssignmentCreate.js
+++ AssignmentCreate.js
@@ -1,7 +1,7 @@
 export default {
     template: `
-    <form @submit.prevent="add" class="border border-gray-300 rounded-md p-1 bg-gray-300">
-        <input v-model="newTask" class="bg-white p-1 mr-2 rounded-md" type="text" placeholder="Enter a task..." />
+    <form @submit.prevent="add" class="flex justify-between border border-gray-300 rounded-md p-1 bg-gray-300">
+        <input v-model="newTask" class="flex-grow bg-white p-1 mr-2 rounded-md" type="text" placeholder="Enter a task..." />
         <button type="submit" class="bg-gray-500 text-white p-2 rounded-md cursor-pointer hover:bg-gray-400">Submit</button>
     </form>
     `,
