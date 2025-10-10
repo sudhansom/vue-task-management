27af6ea46b2dabd432d1f0431335a9a10f44ea0c
bhuwan <bkspoudel@gmail.com>
2025-10-10 11:40:41 +0200

    add a component to make a custom text-area

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import Assignments from "./Assignments.js"
+import TabbableTextarea from "./TabbableTextarea.js"
+
 export default {
     components: {
         Assignments,
+        'tabbable-textarea': TabbableTextarea
     },
     template: `
         <assignments />
-
+        <tabbable-textarea />
     `,
     data(){
         return {
---
+++ TabbableTextarea.js
@@ -0,0 +1,7 @@
+export default {
+    template: `
+        <h1>Tabbable Text Area</h1>
+        <textarea name="" cols="30" rows="10" class="border border-gray-500 h-20 rounded-md p-2"></textarea>
+
+    `
+}
