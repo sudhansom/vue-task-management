03ab2a4a16bc985f1443fdea98b618322a1ee1a2
bhuwan <bkspoudel@gmail.com>
2025-10-10 11:49:01 +0200

    create an event listener for tab keydown

--- TabbableTextarea.js
+++ TabbableTextarea.js
@@ -1,7 +1,20 @@
 export default {
     template: `
         <h1>Tabbable Text Area</h1>
-        <textarea name="" cols="30" rows="10" class="border border-gray-500 h-20 rounded-md p-2"></textarea>
+        <textarea @keydown.tab.prevent="tabbableTextarea" name="textArea" v-model="comment" cols="30" rows="10" class="border border-gray-500 h-20 rounded-md p-2"></textarea>
 
-    `
+    `,
+    data(){
+        return {
+            comment: ''
+        }
+    },
+    methods: {
+        tabbableTextarea(e){
+
+            let value = e.target.value;
+
+            console.log(value);
+        }
+    }
 }
